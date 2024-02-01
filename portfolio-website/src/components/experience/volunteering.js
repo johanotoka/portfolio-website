34d008f7d515c094324a23db4e9bb2d7b045955f
Steeve-Johan Otoka-Eyota <jotok15@my.yorku.ca>
2024-02-01 03:05:14 -0500

    added more volunteering positions

--- portfolio-website/src/components/experience/volunteering.js
+++ portfolio-website/src/components/experience/volunteering.js
@@ -8,9 +8,58 @@ export const volunteering_positions = [
     },
     {
         title: 'IT Executive',
+        date: 'Dec. 2023 – Present',
+        company: 'ElleHacks',
+        location: 'Toronto, ON',
+        description: ["Develop the ElleHacks 2024 website using React, Bootstrap, and Tailwind CSS"]
+    },
+    {
+        title: 'IT Executive',
         date: 'Oct. 2022 – Feb. 2023',
         company: 'ElleHacks',
         location: 'Toronto, ON',
         description: ["Used Figma to design web site mock-ups for ElleHacks 2023", "Developed the ElleHacks 2023 website using HTML, CSS, and JavaScript", "Worked in a team using the Agile methodology for constant collaboration and continuous improvement at every stage of the project"]
-    }
+    },
+    {
+        title: 'General Executive',
+        date: 'Sep. 2022 – Apr. 2023',
+        company: 'GDSC York University',
+        location: 'Toronto, ON',
+        description: []
+    },
+    {
+        title: 'Computer Engineering Representative',
+        date: 'Jul. 2022 – Apr. 2023',
+        company: 'Lassonde Engineering Society',
+        location: 'Toronto, ON',
+        description: []
+    },
+    {
+        title: 'VP Event',
+        date: 'May 2022 – Apr. 2023',
+        company: 'CSHub',
+        location: 'Toronto, ON',
+        description: []
+    },
+    {
+        title: 'IT Executive',
+        date: 'Dec 2021 – Apr. 2022',
+        company: 'ElleHacks',
+        location: 'Toronto, ON',
+        description: [" Maintained and updated the ElleHacks official website using HTML5, CSS3 and JavaScript", "Used Figma mockups for prototyping designs of the updated website", "Collaborated on GitHub using an Agile methodology to solve issues and complete assigned tasks"]
+    },
+    {
+        title: 'Store Manager',
+        date: 'Sep. 2021 – May 2022',
+        company: 'Lassonde Engineering Society',
+        location: 'Toronto, ON',
+        description: ["Planed and promoted the Lassonde Engineering Society’s merchandise on social media and sold through LES's website"]
+    },
+    {
+        title: 'Community Lead',
+        date: 'Aug. 2021 – Aug. 2022',
+        company: 'Google Developer Student Clubs',
+        location: 'Toronto, ON',
+        description: ["Worked on marketing and promoting Google DSC events and activities through social media", "Built and maintain constructive relationships with 3 other student-run clubs by co-hosting events", "Promoted and advertised YuHacks 2022 and reached 300+ people", "Co-hosted and moderated YuHacks 2022 workshops streamed by 80+ viewers"]
+    },
 ];
