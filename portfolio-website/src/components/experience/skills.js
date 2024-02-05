a46b34921e874826b35639ad83abc9b5cc468085
Steeve-Johan Otoka-Eyota <jotok15@my.yorku.ca>
2024-02-04 19:08:14 -0500

    Create skills.js

---
+++ portfolio-website/src/components/experience/skills.js
@@ -0,0 +1,76 @@
+export const skills = [
+    {
+        type: 'Frontend Development',
+        listing: [
+            {
+                skill: 'HTML',
+                level: 'Experienced'
+            },
+            {
+                skill: 'CSS',
+                level: 'Experienced'
+            },
+            {
+                skill: 'Sass',
+                level: 'Experienced'
+            },
+            {
+                skill: 'JavaScript',
+                level: 'Experienced'
+            },
+            {
+                skill: 'React',
+                level: 'Experienced'
+            },
+            {
+                skill: 'Bootstrap',
+                level: 'Experienced'
+            },
+            {
+                skill: 'Material UI',
+                level: 'Experienced'
+            },
+            {
+                skill: 'Tailwind CSS',
+                level: 'Experienced'
+            },
+        ]
+    },
+    {
+        type: 'Backend Development',
+        listing: [
+            {
+                skill: 'Java',
+                level: 'Experienced'
+            },
+            {
+                skill: 'Python',
+                level: 'Experienced'
+            },
+            {
+                skill: 'NodeJS',
+                level: 'Experienced'
+            },
+            {
+                skill: 'MySQL',
+                level: 'Experienced'
+            },
+            {
+                skill: 'MongoDB',
+                level: 'Experienced'
+            },
+            {
+                skill: 'Python',
+                level: 'Experienced'
+            },
+            {
+                skill: 'C#',
+                level: 'Intermediate'
+            },
+            {
+                skill: 'C/C++',
+                level: 'Intermediate'
+            },
+        ]
+    }
+];
